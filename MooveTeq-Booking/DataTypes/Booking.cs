6059b1473a06d32065ac7760c4f2262fa1bc7d32
jonasluehrig <jonas@jonaslue.de>
2021-06-03 13:40:56 +0200

    Projektdateien hinzufügen.

---
+++ MooveTeq-Booking/DataTypes/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EFTest.DataTypes {
+    public class Booking {
+        public int BookingId { get; set; }
+        public Customer Customer { get; set; }
+        public int CustomerId{ get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int? TripDistance { get; set; }
+        public double TotalTripCost { get; set; }
+        public int? TripDiscountPercentage { get; set; }
+    }
+}
---
+++ MooveTeq-Booking/DataTypes/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace EFTest.DataTypes {
+    public class Customer {
+        public int CustomerId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public List<Booking> Bookings { get; } = new List<Booking>();
+    }
+}
