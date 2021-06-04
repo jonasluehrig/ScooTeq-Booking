da6bd785b6e50195c4525931abd096a1a663a915
jerptrs <jerptrs@gmail.com>
2021-06-04 13:28:39 +0200

    smol refactor

--- MooveTeq-Booking/Data/Booking.cs
+++ MooveTeq-Booking/Data/Booking.cs
@@ -3,12 +3,19 @@
 namespace MooveTeqBooking.Data {
     public class Booking {
         public int BookingId { get; set; }
+
         public Customer Customer { get; set; }
+
         public int CustomerId{ get; set; }
+
         public DateTime? StartTime { get; set; }
+
         public DateTime? EndTime { get; set; }
+
         public int? TripDistance { get; set; }
+
         public double TotalTripCost { get; set; }
+
         public int? TripDiscountPercentage { get; set; }
     }
 }
